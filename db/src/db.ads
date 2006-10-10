b0c82991e201d844db5560dea4171967bbc98644
Pascal Obry <pascal@obry.net>
2006-10-10 17:02:18 +0200

    Initial revison of the DB abstract binding.

--- db/src/db.ads
+++ db/src/db.ads
@@ -19,8 +19,67 @@
 --  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
 ------------------------------------------------------------------------------
 
+with Ada.Containers.Indefinite_Vectors;
+
 package DB is
 
-   pragma Pure;
+   use Ada;
+
+   DB_Error : exception;
+   --  Raised for all errors reported by the database
+
+   type Handle is abstract tagged private;
+
+   --  Open / Close
+
+   procedure Connect
+     (DB       : in out Handle;
+      Name     : in String;
+      User     : in String := "";
+      Password : in String := "");
+   --  Open the database named Name
+
+   procedure Close (DB : in Handle);
+   --  Close the current database
+
+   --  Transaction
+
+   procedure Begin_Transaction (DB : in Handle);
+   --  Start a new transaction, do not support embedded transactions
+
+   procedure Commit (DB : in Handle);
+   --  Commit the current transaction
+
+   procedure Rollback (DB : in Handle);
+   --  Rollback the current transaction
+
+   --  Statement
+
+   type Iterator is abstract tagged private;
+
+   package String_Vectors is
+     new Containers.Indefinite_Vectors (Natural, String);
+
+   procedure Prepare_Select
+     (DB   : in     Handle;
+      Iter : in out Iterator'Class;
+      SQL  : in     String);
+   --  Prepare a select statement (SQL must be a select command)
+
+   function Get_Line
+     (Iter : in Iterator) return String_Vectors.Vector;
+   --  Returns the current row and move to the next one
+
+   function More (Iter : in Iterator) return Boolean;
+   --  Returns True if there is more data (row) to fetch
+
+   procedure Execute (DB : in Handle; SQL : in String);
+   --  Execute SQL request into DB
+
+private
+
+   type Handle is abstract tagged null record;
+
+   type Iterator is abstract tagged null record;
 
 end DB;
