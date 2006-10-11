4e10fdefbc32a63a71208e726b273d31e6b1adbb
Pascal Obry <pascal@obry.net>
2006-10-11 16:11:53 +0200

    Adjust slightly the DB interface. First implementation of the SQLite3 support. Minor reformatting.

--- web/src/vision2pixels.adb
+++ web/src/vision2pixels.adb
@@ -2,7 +2,7 @@
 --                              Vision2Pixels                               --
 --                                                                          --
 --                           Copyright (C) 2006                             --
---                      Olivier Ramonat - Pascal Obry                       --
+--                      Pascal Obry - Olivier Ramonat                       --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
 --  it under the terms of the GNU General Public License as published by    --
