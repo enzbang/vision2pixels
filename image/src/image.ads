fc8e35c7b0b220538a56725dd2920376c886b33b
Olivier Ramonat <olivier@ramonat.fr>
2006-10-13 23:25:06 +0200

    Adds new controlled type Image_Data
    Image_Data creates Image_Info_Ptr and Image_Ptr
    Thumbnails are created on Init_Image_Data
    Image_Info_Ptr and Image_Ptr are destroyed automatically when Image_Data
    complete its scope of execution

--- image/src/image.ads
+++ image/src/image.ads
@@ -2,7 +2,7 @@
 --                              Vision2Pixels                               --
 --                                                                          --
 --                           Copyright (C) 2006                             --
---                      Olivier Ramonat - Pascal Obry                       --
+--                      Pascal Obry - Olivier Ramonat                       --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
 --  it under the terms of the GNU General Public License as published by    --
@@ -19,8 +19,31 @@
 --  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
 ------------------------------------------------------------------------------
 
+with Ada.Finalization;
+with G2F;
+with G2F.IO;
+
 package Image is
 
-   pragma Pure;
+   type Image_Data is private;
+
+   Thumbnail_Size : constant G2F.IO.Image_Size := (150, 150);
+
+   procedure Initialize (Img : in out Image_Data);
+   --  Initialize Image_Ptr and Image_Info_Ptr structures
+
+   procedure Finalize (Img : in Image_Data);
+   --  Destroys Image
+
+   procedure Init_Image_Data (Img : in out Image_Data; Filename : in String);
+   --  Set image filename, read image info and create thumbnail
+
+private
+
+   type Image_Data is new Ada.Finalization.Controlled with
+      record
+         Info_Ptr  : G2F.Image_Info_Ptr;
+         Image_Ptr : G2F.Image_Ptr;
+      end record;
 
 end Image;
