2ec56ecda9de5a4e8ac94bc6a9504ca6d57cfbc6
Olivier Ramonat <olivier@ramonat.fr>
2006-10-23 23:02:47 +0200

    Create a minimal set of data

--- db/data/schema-sqlite.sql
+++ db/data/schema-sqlite.sql
@@ -5,15 +5,15 @@ create table "user" (
 );
 
 create table "template" (
-"id" int not null primary key,
+"id" integer not null primary key autoincrement,
 "filename" varchar(512) not null unique,
 "description" longtext not null
 );
 
 create table "comment" (
-"id" int not null primary key,
+"id" integer not null primary key autoincrement,
 "date" date not null ,
-"parent" int null ,
+"parent" integer null ,
 "user_login" varchar( 50 ) not null ,
 "comment" longtext not null ,
 "filename" varchar( 512 ) null ,
@@ -22,35 +22,35 @@ foreign key ("parent") references comment("id")
 );
 
 create table "category" (
-"id" int not null primary key,
+"id" integer not null primary key autoincrement,
 "name" varchar(100) not null
 );
 
 create table "photo" (
-"id" int not null primary key,
+"id" integer not null primary key autoincrement,
 "name" varchar(100) not null,
 "filename" varchar(512) not null unique,
-"category_id" int not null,
+"category_id" integer not null,
 "date_publication" date not null,
-"last_comment_id" int,
-"template_id" int not null,
-"visit_counter" int not null,
-"comment_counter" int not null,
+"last_comment_id" integer,
+"template_id" integer not null,
+"visit_counter" integer not null,
+"comment_counter" integer not null,
 foreign key ("category_id") references category("id"),
 foreign key ("last_comment_id") references comment("id"),
 foreign key ("template_id") references template_id("id")
 );
 
 create table "photo_comment" (
-"photo_id" int not null,
-"comment_id" int not null unique,
+"photo_id" integer not null,
+"comment_id" integer not null unique,
 foreign key ("photo_id") references photo("id"),
 foreign key ("comment_id") references comment("id")
 );
 
 create table "user_photo" (
 "user_login" varchar(50) not null,
-"photo_id" int not null,
+"photo_id" integer not null,
 foreign key ("photo_id") references photo("id"),
 foreign key ("user_login") references user("login")
 );
