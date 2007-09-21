007ac50aade1a4705acc87aea31a237031ce8df1
olivier.ramonat <olivier.ramonat@33d37de6-082f-0410-8f52-fde41cb23233>
2007-09-21 14:50:22 +0000

    Adds new function V2P.URL.User_Name to extract User_Name from a URL
    git-svn-id: https://vision2pixels.googlecode.com/svn/trunk@106 33d37de6-082f-0410-8f52-fde41cb23233

---
+++ web/src/v2p-url.ads
@@ -0,0 +1,27 @@
+------------------------------------------------------------------------------
+--                              Vision2Pixels                               --
+--                                                                          --
+--                           Copyright (C) 2007                             --
+--                      Pascal Obry - Olivier Ramonat                       --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.       --
+------------------------------------------------------------------------------
+
+package V2P.URL is
+
+   function User_Name (URL : in String) return String;
+   --  Returns User_Name from URL
+
+end V2P.URL;
