38b6011b86e46d774ffe37368a7928250c863182
Olivier Ramonat <olivier@ramonat.fr>
2007-09-21 14:50:22 +0000

    Adds new function V2P.URL.User_Name to extract User_Name from a URL

---
+++ web/src/v2p-url.adb
@@ -0,0 +1,37 @@
+------------------------------------------------------------------------------
+--                              Vision2Pixels                               --
+--                                                                          --
+--                           Copyright (C) 2007                             --
+--                      Pascal Obry - Olivier Ramonat                       --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.       --
+------------------------------------------------------------------------------
+
+with V2P.Template_Defs.User_Page;
+
+package body V2P.URL is
+
+   use V2P;
+
+   ---------------
+   -- User_Name --
+   ---------------
+
+   function User_Name (URL : in String) return String is
+   begin
+      return URL (URL'First + Template_Defs.User_Page.URL'Length .. URL'Last);
+   end User_Name;
+
+end V2P.URL;
