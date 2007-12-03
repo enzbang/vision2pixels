8bdf3d4595f8dbba0abd33f14f7f643c2a35c816
Pascal Obry <pascal@obry.net>
2007-12-03 15:17:25 +0100

    Add non regression test for posting photos and messages.

--- web/test/src/web_tests-post.adb
+++ web/test/src/web_tests-post.adb
@@ -23,8 +23,11 @@ with AWS.Client;
 with AWS.Response;
 with AWS.Utils;
 
-with V2P.Template_Defs.Page_Forum_Threads;
 with V2P.Template_Defs.Block_Forum_Filter;
+with V2P.Template_Defs.Block_Login;
+with V2P.Template_Defs.Page_Forum_New_Photo_Entry;
+with V2P.Template_Defs.Page_Forum_New_Text_Entry;
+with V2P.Template_Defs.Page_Forum_Threads;
 
 package body Web_Tests.Post is
 
@@ -34,10 +37,28 @@ package body Web_Tests.Post is
    --  The very first thing to do is to get the main page
 
    procedure Close (T : in out AUnit.Test_Cases.Test_Case'Class);
-   --  Close the Web connection
+   --  Closes the Web connection
 
    procedure Check_Post (T : in out AUnit.Test_Cases.Test_Case'Class);
-   --  List threads in a forum
+   --  Checks a single post in a forum
+
+   procedure Check_New_Post (T : in out AUnit.Test_Cases.Test_Case'Class);
+   --  Checks the new post, must be called after Post_New_Photo
+
+   procedure Post_New_Photo (T : in out AUnit.Test_Cases.Test_Case'Class);
+   --  Posts a new photo
+
+   procedure Post_New_Message (T : in out AUnit.Test_Cases.Test_Case'Class);
+   --  Posts a new photo
+
+   function URL_Context return String;
+   --  Returns the context as an HTTP URL parameter
+
+   procedure Login (User, Password : in String);
+   --  Login the specified user
+
+   procedure Logout;
+   --  Logout current connected user
 
    Connection : Client.HTTP_Connection;
    --  Server connection used by all tests
@@ -55,6 +76,24 @@ package body Web_Tests.Post is
       Client.Close (Connection);
    end Close;
 
+   --------------------
+   -- Check_New_Post --
+   --------------------
+
+   procedure Check_New_Post (T : in out AUnit.Test_Cases.Test_Case'Class) is
+      use V2P.Template_Defs;
+      Result : Response.Data;
+   begin
+      Client.Get
+        (Connection, Result, URI => "/forum/entry?TID=142&" & URL_Context);
+
+      Check
+        (Response.Message_Body (Result),
+         Word_Set'(+"Un_Troll", +"Un_Troll", +"troll.jpg",
+           +"non révélé", +"un<em>petit</em>troll"),
+         "wrong entry for post 142" & Response.Message_Body (Result));
+   end Check_New_Post;
+
    ----------------
    -- Check_Post --
    ----------------
@@ -63,21 +102,7 @@ package body Web_Tests.Post is
      (T : in out AUnit.Test_Cases.Test_Case'Class)
    is
       use V2P.Template_Defs;
-
       Result : Response.Data;
-
-      function URL_Context return String;
-      --  Returns the context as an HTTP URL parameter
-
-      -----------------
-      -- URL_Context --
-      -----------------
-
-      function URL_Context return String is
-      begin
-         return "CTX_WB=" & To_String (Context);
-      end URL_Context;
-
    begin
       Client.Get
         (Connection, Result, URI => "/forum/entry?TID=89&" & URL_Context);
@@ -91,6 +116,48 @@ package body Web_Tests.Post is
            +"L'auteur", +"Mais encore ?"),
          "wrong entry for post 89");
    end Check_Post;
+
+   -----------
+   -- Login --
+   -----------
+
+   procedure Login (User, Password : in String) is
+      use V2P.Template_Defs;
+
+      function Login_Parameters (Login, Password : in String) return String;
+      --  Returns the HTTP login parameters
+
+      ----------------------
+      -- Login_Parameters --
+      ----------------------
+
+      function Login_Parameters (Login, Password : in String) return String is
+      begin
+         return '&' & Block_Login.HTTP.LOGIN & '=' & Login &
+           '&' & Block_Login.HTTP.PASSWORD & '=' & Password;
+      end Login_Parameters;
+
+      Result : Response.Data;
+
+   begin
+      Client.Get
+        (Connection, Result,
+         URI => Block_Login.Ajax.onclick_bl_login_form_enter &
+         '?' & URL_Context & Login_Parameters (User, Password));
+   end Login;
+
+   ------------
+   -- Logout --
+   ------------
+
+   procedure Logout is
+      use V2P.Template_Defs;
+      Result : Response.Data;
+   begin
+      Client.Get
+        (Connection, Result,
+         URI => Block_Login.Ajax.onclick_bl_logout_enter & '?' & URL_Context);
+   end Logout;
 
    ---------------
    -- Main_Page --
@@ -129,6 +196,189 @@ package body Web_Tests.Post is
    end Name;
 
    --------------------
+   -- Post_New_Photo --
+   --------------------
+
+   procedure Post_New_Photo (T : in out AUnit.Test_Cases.Test_Case'Class) is
+      use V2P.Template_Defs;
+      Result : Response.Data;
+   begin
+      Logout;
+
+      --  Go to the upload page (anonymous)
+
+      Client.Get (Connection, Result, URI => "/add_photo?" & URL_Context);
+
+      Check_Anonymous_Upload_Page : declare
+         use AUnit.Assertions;
+         Page : constant String := Response.Message_Body (Result);
+      begin
+         Check
+           (Page,
+            Word_Set'(1 => +"Vous devez être connecté pour poster un message"),
+            "Must be connected to upload a photo");
+      end Check_Anonymous_Upload_Page;
+
+      --  Go to the upload page (turbo)
+
+      Login ("turbo", "turbopass");
+
+      Client.Get (Connection, Result, URI => "/add_photo?" & URL_Context);
+
+      Check_Turbo_Upload_Page : declare
+         use AUnit.Assertions;
+         Page : constant String := Response.Message_Body (Result);
+      begin
+         Check
+           (Page,
+            Word_Set'(1 => +"Vous devez attendre jusqu'au"),
+            "Turbo should not have the right to upload at this time");
+      end Check_Turbo_Upload_Page;
+
+      --  Go to the upload page (test)
+
+      Logout;
+      Login ("test", "test");
+
+      Client.Get (Connection, Result, URI => "/add_photo?" & URL_Context);
+
+      Check_Upload_Page : declare
+         use AUnit.Assertions;
+         Page : constant String := Response.Message_Body (Result);
+      begin
+         Check
+           (Page,
+            Word_Set'(1 => +"Ajouter une nouvelle photo",
+                      2 => +"<form",
+                      3 => +"/forum/new_photo_entry",
+                      4 => +"<input",
+                      5 => +"FILENAME",
+                      6 => +"<input",
+                      7 => +"ENTER",
+                      8 => +"submit"),
+            "cannot get the add_photo (for uploading) page");
+      end Check_Upload_Page;
+
+      --  Upload new photo
+
+      Client.Upload
+        (Connection, Result,
+         Filename => "./troll.jpg",
+         URI      => "/forum/new_photo_entry?" & URL_Context
+         & "&FILENAME=troll.jpg");
+
+      Check_Post_Page : declare
+         use AUnit.Assertions;
+         Page : constant String := Response.Message_Body (Result);
+      begin
+         Check
+           (Page,
+            Word_Set'(1 => +"Choisir un forum",
+                      2 => +"Forum photographies",
+                      3 => +"<select",
+                      4 => +"CATEGORY",
+                      5 => +"Titre",
+                      6 => +"test",
+                      7 => +"Ajouter un commentaire"),
+            "Upload has failed");
+      end Check_Post_Page;
+
+      --  Post the photo now into the corresponding forum
+
+      Client.Get
+        (Connection, Result,
+         URI => Page_Forum_New_Photo_Entry.
+           Ajax.onsubmit_pfnpe_new_entry_form_submit & '?' & URL_Context
+         & "&NAME=Un_Troll&comment_input=un_petit_troll"
+         & "&PID=89&CATEGORY=1");
+
+      Check_Forum_Page : declare
+         use AUnit.Assertions;
+         Page : constant String := Response.Message_Body (Result);
+      begin
+         Check
+           (Page,
+            Word_Set'(1 => +"<location",
+                      2 => +"/forum/entry?",
+                      3 => +"TID",
+                      4 => +"142"),
+            "Not returned on the proper forum after upload");
+      end Check_Forum_Page;
+   end Post_New_Photo;
+
+   ----------------------
+   -- Post_New_Message --
+   ----------------------
+
+   procedure Post_New_Message (T : in out AUnit.Test_Cases.Test_Case'Class) is
+      use V2P.Template_Defs;
+      Result : Response.Data;
+   begin
+      Logout;
+
+      --  Go to the post test page (anonymous)
+
+      Client.Get
+        (Connection, Result, URI => "/forum/new_entry?" & URL_Context);
+
+      Check_Anonymous_Post_Page : declare
+         use AUnit.Assertions;
+         Page : constant String := Response.Message_Body (Result);
+      begin
+         Check
+           (Page,
+            Word_Set'(1 => +"Vous devez être connecté pour poster un message"),
+            "Must be connected to upload a photo");
+      end Check_Anonymous_Post_Page;
+
+      --  Go to the post text page (turbo)
+
+      Login ("turbo", "turbopass");
+
+      Client.Get
+        (Connection, Result, URI => "/forum/new_entry?" & URL_Context);
+
+      Check_Post_Page : declare
+         use AUnit.Assertions;
+         Page : constant String := Response.Message_Body (Result);
+      begin
+         Check
+           (Page,
+            Word_Set'(1 => +"Poster un nouveau message",
+                      2 => +"Sélection du forum",
+                      3 => +"<select",
+                      4 => +"MatCat1",
+                      5 => +"MatCat2",
+                      6 => +"turbo",
+                      7 => +"ENTER",
+                      8 => +"submit"),
+            "cannot get the post message page");
+      end Check_Post_Page;
+
+      --  Post the message
+
+      Client.Get
+        (Connection, Result,
+         URI => Page_Forum_New_Text_Entry.
+           Ajax.onsubmit_pfnte_new_entry_form_submit & '?' & URL_Context
+         & "&NAME=""une vente""&comment_input=""une sourie pour DELL M70"""
+         & "&CATEGORY=7");
+
+      Check_Forum_Page : declare
+         use AUnit.Assertions;
+         Page : constant String := Response.Message_Body (Result);
+      begin
+         Check
+           (Page,
+            Word_Set'(1 => +"<location",
+                      2 => +"/forum/entry?",
+                      3 => +"TID",
+                      4 => +"143"),
+            "Not returned on the proper forum after posting a message");
+      end Check_Forum_Page;
+   end Post_New_Message;
+
+   --------------------
    -- Register_Tests --
    --------------------
 
@@ -137,7 +387,19 @@ package body Web_Tests.Post is
    begin
       Register_Routine (T, Main_Page'Access, "main page");
       Register_Routine (T, Check_Post'Access, "check post");
+      Register_Routine (T, Post_New_Photo'Access, "post new photo");
+      Register_Routine (T, Check_New_Post'Access, "check new post");
+      Register_Routine (T, Post_New_Message'Access, "post new photo");
       Register_Routine (T, Close'Access, "close connection");
    end Register_Tests;
+
+   -----------------
+   -- URL_Context --
+   -----------------
+
+   function URL_Context return String is
+   begin
+      return "CTX_WB=" & To_String (Context);
+   end URL_Context;
 
 end Web_Tests.Post;
