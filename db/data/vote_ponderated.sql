747a95f05aa95bb80587aac73335870f18610a23
Olivier Ramonat <olivier@ramonat.fr>
2007-12-23 12:35:21 +0100

    Add vote_ponderated.sql and vote.sql

---
+++ db/data/vote_ponderated.sql
@@ -0,0 +1,7 @@
+insert into vote_ponderated values (1);
+insert into vote_ponderated values (0.75);
+insert into vote_ponderated values (0.61);
+insert into vote_ponderated values (0.52);
+insert into vote_ponderated values (0.45);
+insert into vote_ponderated values (0.40);
+insert into vote_ponderated values (0.37);
