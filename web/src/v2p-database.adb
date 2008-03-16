1b656ef28fd894767927ea87460d2a4f6126c86e
Olivier Ramonat <olivier@ramonat.fr>
2008-03-16 16:33:28 +0100

    Fix 'last commented' filter
    New photo without comments are considered as comments in the 'last commented' filter

--- web/src/v2p-database.adb
+++ web/src/v2p-database.adb
@@ -1611,6 +1611,8 @@ package body V2P.Database is
             when Last_Commented =>
                Append (Select_Stmt, " order by post.last_comment_id");
                Append (Select_Stmt, ' ' & Order_Direction'Image (Order_Dir));
+               Append (Select_Stmt, ", post.date_post");
+               Append (Select_Stmt, ' ' & Order_Direction'Image (Order_Dir));
 
             when Best_Noted =>
                Append (Select_Stmt, " order by sum_rating");
