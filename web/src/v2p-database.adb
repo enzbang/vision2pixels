c0607af4e712d7b7dd62b78213802263d92a9fe5
Pascal Obry <pascal@obry.net>
2008-04-15 13:38:51 +0200

    Fix casing of SQL keywords.

--- web/src/v2p-database.adb
+++ web/src/v2p-database.adb
@@ -2561,7 +2561,7 @@ package body V2P.Database is
       DBH.Handle.Prepare_Select
         (Iter,
          "SELECT * FROM user "
-         & "WHERE login=" & Q (Login) & " or email=" & Q (Email));
+         & "WHERE login=" & Q (Login) & " OR email=" & Q (Email));
 
       if Iter.More then
          Iter.End_Select;
@@ -2574,7 +2574,7 @@ package body V2P.Database is
       DBH.Handle.Prepare_Select
         (Iter,
          "SELECT * FROM user_to_validate "
-         & "WHERE login=" & Q (Login) & " or email=" & Q (Email));
+         & "WHERE login=" & Q (Login) & " OR email=" & Q (Email));
 
       if Iter.More then
          Iter.End_Select;
