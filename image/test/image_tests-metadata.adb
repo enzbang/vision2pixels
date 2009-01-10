9119963495e201a8197f5c57e0e03b61e97fd721
Olivier Ramonat <olivier@ramonat.fr>
2009-01-10 17:04:02 +0100

    Fix Geo_Coordinate test

--- image/test/image_tests-metadata.adb
+++ image/test/image_tests-metadata.adb
@@ -37,8 +37,8 @@ package body Image_Tests.Metadata is
    procedure Geo_Format (T : in out AUnit.Test_Cases.Test_Case'Class) is
       pragma Unreferenced (T);
 
-      Geo_Lat   : Geo_Coordinate := 48.864263;
-      Geo_Long  : Geo_Coordinate := 2.397927;
+      Geo_Lat   : Geo_Coordinate := Geo_Coordinate'Value ("48.864263");
+      Geo_Long  : Geo_Coordinate := Geo_Coordinate'Value ("2.39806");
 
       Pos_Lat  : Latitude;
       Pos_Long : Longitude;
@@ -46,25 +46,26 @@ package body Image_Tests.Metadata is
       Get_Lat  : Geo_Coordinate;
       Get_Long : Geo_Coordinate;
    begin
-      --  48.864263, 2.397927
-
       Pos_Lat.Format (Geo_Lat);
       Pos_Long.Format (Geo_Long);
 
       Assert (Pos_Lat = Latitude'(48, 51, 51, N),
-              "Error in latitude format for (48, 51, 51, N)");
+              "Error in latitude format for (48, 51, 51, N)"
+             & "Get : " & Image.Metadata.Geographic.Image (Pos_Lat));
 
       Get_Lat := To_Geo_Coordinate (Pos_Lat);
 
-      Assert (Get_Lat = Geo_Lat,
-              "To_Geo_Coordinate error for 48° 51' 51'' N");
+      Assert (Get_Lat - Geo_Lat < Geo_Coordinate'Small,
+              "To_Geo_Coordinate error for 48° 51' 51'' N" &
+             Image.Metadata.Geographic.Image (Pos_Lat));
 
       Assert (Pos_Long = Longitude'(2, 23, 53, E),
-              "Error in longitude format (2, 23, 53, E)");
+              "Error in longitude format (2, 23, 53, E)" &
+              Image.Metadata.Geographic.Image (Pos_Lat));
 
       Get_Long := To_Geo_Coordinate (Pos_Long);
 
-      Assert (Get_Long = Geo_Long,
+      Assert (Get_Long - Geo_Long < Geo_Coordinate'Small,
               "To_Geo_Coordinate error for 2° 23' 53'' E");
 
       Assert (Image.Metadata.Geographic.Image (Pos_Lat) = "N 48° 51' 51",
@@ -73,10 +74,8 @@ package body Image_Tests.Metadata is
       Assert (Image.Metadata.Geographic.Image (Pos_Long) = "E 2° 23' 53",
               "Error with " & Image.Metadata.Geographic.Image (Pos_Long));
 
-      --  -87.728055, -40.4461110
-
-      Geo_Long  := -87.728055;
-      Geo_Lat   := -40.4461110;
+      Geo_Long  := Geo_Coordinate'Value ("-87.728055");
+      Geo_Lat   := Geo_Coordinate'Value ("-40.4461110");
 
       Pos_Lat.Format (Geo_Lat);
       Pos_Long.Format (Geo_Long);
@@ -90,10 +89,10 @@ package body Image_Tests.Metadata is
       Get_Lat  := To_Geo_Coordinate (Pos_Lat);
       Get_Long := To_Geo_Coordinate (Pos_Long);
 
-      Assert (Get_Lat = Geo_Lat,
+      Assert (Get_Lat - Geo_Lat < Geo_Coordinate'Small,
               "To_Geo_Coordinate error for 40° 26' 46'' S");
 
-      Assert (Get_Long = Geo_Long,
+      Assert (Get_Long - Geo_Long < Geo_Coordinate'Small,
               "To_Geo_Coordinate error for 87° 43' 41'' W");
 
       Assert (Image.Metadata.Geographic.Image (Pos_Lat) = "S 40° 26' 46",
