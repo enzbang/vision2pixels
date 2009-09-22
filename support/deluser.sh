2e071464c5eb34220192a425fc6bd721e450ec52
Pascal Obry <pascal@obry.net>
2009-09-22 18:49:01 +0200

    New script to delete a user from the database

---
+++ support/deluser.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ "$1" = "" ]; then
+    echo $0 login
+    exit 1
+fi
+
+cp v2p.db "v2p.db.$(date)"
+
+# remove e-mail and password
+
+echo "update user set password='password', email='nobody@nowhere.com' where login='"$1"';" | sqlite3 v2p.db
+
+echo "update user_page set content='Compte désactivé', content_html='Compte désactivé' where user_login='"$1"';" | sqlite3 v2p.db
