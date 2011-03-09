08353ec639cbdf6a49620f935d27058d55a2adfe
Pascal Obry <pascal@obry.net>
2011-03-09 14:22:20 +0100

    Add new forum LeBar.
    Add corresponding migration script.

---
+++ db/scripts/migrate-lebar.sql
@@ -0,0 +1,4 @@
+insert into forum
+       values (5, "LeBar", "Discussions à bâtons rompus jusqu'au bout de la nuit...", datetime(current_timestamp), "FALSE", "FALSE");
+
+insert into category values (20, 5, 'Divers');
