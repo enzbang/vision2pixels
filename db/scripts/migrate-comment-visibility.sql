f2aa893a1129cb314d8ce78687829dcf8c4a3a48
Pascal Obry <pascal@obry.net>
2011-05-26 10:54:33 +0200

    Fix default in migration script.

--- db/scripts/migrate-comment-visibility.sql
+++ db/scripts/migrate-comment-visibility.sql
@@ -1,4 +1,4 @@
-alter table user_preferences add column "start_comment_visible" boolean;
+alter table user_preferences add column "start_comment_visible" boolean default TRUE;
 
 update user_preferences
   set start_comment_visible='TRUE';
